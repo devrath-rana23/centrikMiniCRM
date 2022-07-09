d5366f43714e6c4a25ffbefe2d379ef984647d0b
devrath <devrath.rana@kelltontech.com>
2022-07-09 11:02:04 +0530

    db setup

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <!-- HTML favicon -->
+    <link rel="icon" type="image/x-icon" href="{{asset('assets/img/favicon.ico')}}">
+    
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
